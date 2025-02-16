8578b825cbaf60c8d1a5a071a3638316fe5ca9af
Pranav Srivatsa <pranavsrivatsa@proton.me>
2025-02-16 17:08:30 +0530

    fix: ensured vite does esbuild minify along with other recommendedconfigs for build perf improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,25 @@ import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+// https://vite.dev/config/
 export default defineConfig({
   site: 'https://pranvatsa.github.io',
-  integrations: [vue(), tailwind()]
+  integrations: [vue(), tailwind()],
+  vite: {
+    build: {
+      minify: 'esbuild',
+      target: 'esnext',
+      cssCodeSplit: true,
+      sourcemap: false,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+          },
+        },
+      },
+    },
+  },
 });
